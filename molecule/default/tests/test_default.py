62fb594f77d613421d0778668a8e2877b3f86078
David Reed <david.reedfrancis@sainsburys.co.uk>
2018-02-16 12:55:50 +0000

    [GITHUB-6] Brings in Molecule tests from Scaffold
    Also brings in Makefile, Travis and other scaffold bits.
    
    Removes deprecated tests.

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,10 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_ruby_installed(host):
+    host.package("ruby2.2").is_installed
